5806e955c5cd59b2f8117f48c5d4f4669f0cbd8b
Softcadbury <romain.parage@gmail.com>
2021-03-05 13:54:13 +0100

    Add material-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
+import { Grid, Slider, TextField } from '@material-ui/core';
 import React from 'react';
 import './App.css';
 import Graph from './components/graph';
 
 function App() {
+    const [value, setValue] = React.useState<number | number[]>(30);
+
+    const handleChange = (
+        event: React.ChangeEvent<{}>,
+        newValue: number | number[]
+    ) => {
+        setValue(newValue);
+    };
+
     return (
         <div className="App">
-            <header className="App-header">
-                <h1>Dagga</h1>
-            </header>
+            <TextField
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+            />
+            <Grid container spacing={2}>
+                <Grid item>test</Grid>
+                <Grid item xs>
+                    <Slider
+                        value={value}
+                        onChange={handleChange}
+                        aria-labelledby="continuous-slider"
+                    />
+                </Grid>
+            </Grid>
             <Graph></Graph>
         </div>
     );
