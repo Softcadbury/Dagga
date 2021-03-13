06460912364144eaa003b886ad9e3845efcbd8a4
Softcadbury <romain.parage@gmail.com>
2021-03-13 20:51:56 +0100

    Add eslint and prettier

--- src/views/app.test.tsx
+++ src/views/app.test.tsx
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './app';
 
 test('renders form', () => {
-  render(<App />);
-  const element = screen.getByText(/Période/i);
-  expect(element).toBeInTheDocument();
+    render(<App />);
+    const element = screen.getByText(/Période/i);
+    expect(element).toBeInTheDocument();
 });
